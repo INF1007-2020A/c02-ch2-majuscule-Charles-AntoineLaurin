b2d0ef319f8db39f6b2352370e3f9004be9fdb62
Charles-AntoineLaurin <charles-antoine.laurin@polymtl.ca>
2020-09-14 14:08:11 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -2,12 +2,16 @@
 # -*- coding: utf-8 -*-
 def majuscule(mot):
     resultat = ''
+    lettre = ''
     for lettre in mot:
         # TODO completer la fonction ici
-        
-        resultat += lettre
-    return resultat
+        chiffre = ord(lettre) - 32
+        resultat += chr(chiffre)
 
+    return resultat
+        
+    
+  
 
 if __name__ == '__main__':
     mots = [
